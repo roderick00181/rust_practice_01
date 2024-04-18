697c3bf49fdace2333bbf8b3e36cdd894bd35d27
roderick00181 <roderick00181@gmail.com>
2024-04-18 13:18:58 +0800

    3rd commit

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,8 @@ fn main() {
 		department.push("工程部門".to_string());
 		department.push("業務部門".to_string());
 		
+		let mut employee = HashMap::new();
+		
 		println!("請選擇工作項目");
 		loop {
 			println!("1.新增員工");
@@ -39,18 +41,110 @@ fn main() {
 						println!("{count}.{i}");
 						count += 1;
 					}
-					println!("{count}.新增部門");
+					// println!("{count}.新增部門");
+					println!("{count}.離開");
 					println!("請選擇部門");
-					
-					option = String::new();
-					
-					io::stdin()
-						.read_line(&mut option)
-						.expect("讀取失敗");
+					loop {
+						option = String::new();
 						
+						io::stdin()
+							.read_line(&mut option)
+							.expect("讀取失敗");
+						
+						let option: i32 = match option.trim().parse() {
+							Ok(num) => num,
+							Err(_) => {
+								println!("請輸入選項");
+								continue;
+							}
+						};	
+						
+						if option <= department.len().try_into().unwrap() {
+							println!("請輸入員工姓名");
+							let mut emp_name = String::new();
+							
+							io::stdin()
+								.read_line(&mut emp_name)
+								.expect("讀取失敗");
+								
+							let emp_name = emp_name.trim();
+							
+							let index: usize = (option - 1).try_into().unwrap();
+							
+							let department_name = &department[index].clone();
+							
+							employee.insert(String::from(emp_name), String::from(department_name));
+							
+							println!("將{emp_name}加入{}", department_name);
+							println!();
+							break;
+							
+						// } else if option == (department.len() + 1).try_into().unwrap() {
+						// 	println!("請輸入部門名稱");
+						} else if option == (department.len() + 2).try_into().unwrap() {
+							break;
+						} else {
+							println!("請輸入選項");
+							continue;
+						}
+					}
+					
 				},
-				"2" => println!("太大了！"),
-				"3" => println!("太大了！"),
+				"2" => {
+					let mut count = 1;
+					for i in &department {
+						println!("{count}.{i}");
+						count += 1;
+					}
+					println!("{count}.離開");
+					println!("請選擇部門");
+					loop {
+						option = String::new();
+						
+						io::stdin()
+							.read_line(&mut option)
+							.expect("讀取失敗");
+						
+						let option: i32 = match option.trim().parse() {
+							Ok(num) => num,
+							Err(_) => {
+								println!("請輸入選項");
+								continue;
+							}
+						};	
+						
+						if option <= department.len().try_into().unwrap() {
+							let index: usize = (option - 1).try_into().unwrap();
+							println!("{}", department[index]);
+							for (key, value) in &employee {
+								if value == &department[index] {
+								    println!("{key}");
+								}
+							}
+							println!();
+							break;
+						} else if option == (department.len() + 1).try_into().unwrap() {
+							break;
+						} else {
+							println!("請輸入選項");
+							continue;
+						}
+					}
+					
+				},
+				"3" => {
+					let mut all_employee: Vec<(String, String)> = Vec::new();
+					for (key, value) in &employee {
+						all_employee.push((value.clone(), key.clone()));
+						println!("姓名：{key} 部門：{value}");
+					}
+					println!();
+					all_employee.sort_unstable();
+					for index in &all_employee {
+						println!("姓名：{} 部門：{}", index.1, index.0);
+					}
+					println!();
+				},
 				"4" => break,
 				_ => {
 					println!("請輸入選項");
